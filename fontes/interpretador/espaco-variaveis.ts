d641b10f18812b9c88d41e9591392751f2d6f010
Elender <elenderg@gmail.com>
2023-09-06 10:20:26 -0300

    Update espaco-variaveis.ts
    Incluído sugestão (comentada) para a classe EspacoVariaveis

--- fontes/interpretador/espaco-variaveis.ts
+++ fontes/interpretador/espaco-variaveis.ts
@@ -17,4 +17,41 @@ export class EspacoVariaveis {
         this.valores = {};
         this.resolucoesChamadas = {};
     }
+
+    /*
+    constructor() {
+        this.valores = {};
+        this.resolucoesChamadas = {};
+    }
+
+    // Método para definir o valor de uma variável no espaço de variáveis
+    public setVariavel(nome: string, valor: VariavelInterface): void {
+        this.valores[nome] = valor;
+    }
+
+    // Método para obter o valor de uma variável do espaço de variáveis
+    public getVariavel(nome: string): VariavelInterface | undefined {
+        return this.valores[nome];
+    }
+
+    // Método para verificar se uma variável existe no espaço de variáveis
+    public existeVariavel(nome: string): boolean {
+        return nome in this.valores;
+    }
+
+    // Método para definir uma resolução de chamada
+    public setResolucaoChamada(id: string, resolucao: any): void {
+        this.resolucoesChamadas[id] = resolucao;
+    }
+
+    // Método para obter uma resolução de chamada
+    public getResolucaoChamada(id: string): any {
+        return this.resolucoesChamadas[id];
+    }
+
+    // Método para verificar se uma resolução de chamada existe
+    public existeResolucaoChamada(id: string): boolean {
+        return id in this.resolucoesChamadas;
+    }
+    */
 }
